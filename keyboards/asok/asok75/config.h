884b8fd433e3643e46881753148e69b6ff235621
asok3781 <61459012+asok3781@users.noreply.github.com>
2020-05-11 17:00:14 +1200

    Updated config.h to correct number of underglow LEDs

--- keyboards/asok/asok75/config.h
+++ keyboards/asok/asok75/config.h
@@ -59,7 +59,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #endif
 #define RGB_DI_PIN D0
 #ifdef RGB_DI_PIN
-#define RGBLED_NUM 20
+#define RGBLED_NUM 14
 #define RGBLIGHT_HUE_STEP 8
 #define RGBLIGHT_SAT_STEP 8
 #define RGBLIGHT_VAL_STEP 8
