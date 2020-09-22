9aa502657b5d460406255ace9fd1e25b22a862bb
Puffazz <dakaizhou.gz@gmail.com>
2020-09-22 23:24:18 +1200

    First edit of all the files required for hex qmk firmware compilation

--- keyboards/puffazz_arisu/puffazz_arisu.c
+++ keyboards/puffazz_arisu/puffazz_arisu.c
@@ -1,4 +1,4 @@
-/* Copyright 2020 Aaron Song
+/* Copyright 2020 Greg Zhou
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -13,11 +13,15 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include "asok75.h"
+#include "puffazz_arisu.h"
 
 void matrix_init_kb(void) {
 	// put your keyboard start-up code here
 	// runs once when the firmware starts up
+
+	//setPinOutput(B2); /* Num Lock */
+	//setPinOutput(B1); /* Caps Lock */
+	//setPinOutput(B0); /* Scroll Lock */
 
 	matrix_init_user();
 }
